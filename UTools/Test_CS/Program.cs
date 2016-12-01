ce0351fe4e55e38399af9801880654bfe73f8a1a
Luke_Chen <luke.linus.chen@gmail.com>
2016-12-01 09:56:59 +0800

    添加C#类工程

---
+++ UTools/Test_CS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UTool_CS;
+
+namespace Test_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            if(c.Is64bit())
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
---
+++ UTools/UTool_CS/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UTool_CS
+{
+    public class Class1
+    {
+        public bool Is64bit()
+        {
+            if (Environment.Is64BitProcess)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    
+}
